fe7057e1bb29ed02afdf72f69543eb71d30f5b03
restlucas <lucasouza.doliveira@gmail.com>
2025-01-29 10:45:05 -0300

    fix: changes do deploy

--- server/prisma/migrations/20250126041812_comment_like_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,commentId]` on the table `CommentLike` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `CommentLike_userId_commentId_key` ON `CommentLike`(`userId`, `commentId`);
--- server/prisma/migrations/20250128103005_init_8/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `userId` on the `invitation` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `invitation` DROP COLUMN `userId`;
